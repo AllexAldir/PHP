7e13660472df829567e52ea9bef4de69a8c1fcd9
Állex Campos <allex.assis@ofm.com.br>
2023-07-13 17:32:11 -0300

    Add => Poo
    Criação de primeira classe

--- Poo/classes_obj.php
+++ Poo/classes_obj.php
@@ -1,1 +1,20 @@
 <?php
+
+class Cliente
+{
+	public $nome = 'teste'; //-> visibilidade do atributo
+	public int $idade = 25;
+
+	public function testeMetodo()
+	{
+		echo "Tetse de classe: {$this->idade}";//Acessa um membro do objeto
+	}
+
+	
+}
+
+$C1 = new Cliente(); //-> criando uma classe
+//$C1->testeMetodo(); //-> acessando matodo dentro da classe
+$C1->nome = 'Novo valor'; //-> Reatribuindo valor para o atributo
+$C1->idade = 23; //-> Reatribuindo valor para o atributo
+$C1->testeMetodo();
